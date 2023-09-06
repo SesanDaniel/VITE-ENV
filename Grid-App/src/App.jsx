f4a05c359dc9f8c8584aed8dfc99a1607fb33103
SesanDaniel <obisesan.seyid@gmail.com>
2023-09-06 20:51:29 +0400

    Updated

---
+++ Grid-App/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="grid-container">
+        <div className="grid-items"></div>
+        <div className="grid-items"></div>
+        <div className="grid-items"></div>
+        <div className="grid-items"></div>
+        <div className="grid-items"></div>
+        <div className="grid-items"></div>
+        <div className="grid-items"></div>
+        <div className="grid-items"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
